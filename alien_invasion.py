866ec33ae8a03eda361921509f85e757801d7dd5
sendayang <78051294@qq.com>
2018-09-05 09:29:15 +0800

    2019-09-05 09:28

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+import game_functions as gf
+
+def run_game():
+    #初始化
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    #设置背景色
+    bg_color = (230, 230, 230)
+
+    # 开始游戏主循环
+    while True:
+
+        #监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
