b12aa49071081aa51f657b262a3bceaa839d2a81
sendayang <y78051294@msn.com>
2018-09-06 10:16:13 +0800

    Finish 12

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
-import pygame
 
+import pygame
+ 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 
@@ -11,11 +13,13 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
-
+    pygame.display.set_caption("外星人入侵")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
+
+    # 创建一个用于存储子弹的编组,pygame.sprite.Group 类
+    bullets = Group()
 
     #设置背景色
     bg_color = (230, 230, 230)
@@ -24,9 +28,9 @@ def run_game():
     while True:
 
         #监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship,bullets)
 
 run_game()
